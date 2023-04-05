20a1a0437add5f39762b1cd392bbb40210a4cc83
Emmydrm <emmypee222@gmail.com>
2023-04-05 02:28:17 -0700

    Update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,17 @@
 -- Script that converts hbtn_0c_0 database to UTF8
 -- Convert db 'hbt'
-# Convert hbtn_0c_0 database to UTF8
-echo "Converting hbtn_0c_0 database to UTF8..."
-mysql -e "ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
-# Convert first_table table to UTF8
-echo "Converting first_table table to UTF8..."
-mysql -e "ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+-- Convert table 'first_table'
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
-# Convert name field in first_table table to UTF8
-echo "Converting name field in first_table table to UTF8..."
-mysql -e "ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+-- Convert field 'name' in 'first_table'
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
