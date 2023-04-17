cfcce4191c9a1299dbdf60c101d549ad27d49f86
Emmydrm <emmypee222@gmail.com>
2023-04-17 03:21:09 -0700

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ORM Script
+"""
+import sys
+import sqlalchemy
+~from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+        Column, ForeignKey, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Creates the State class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
