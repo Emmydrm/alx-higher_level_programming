dfe074c82dde659c5eb31dd2af3cc7359a22da6c
Emmydrm <emmypee222@gmail.com>
2023-04-17 02:51:51 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,31 +3,22 @@
 Script that takes in arguments and displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument (safe from MySQL injections).
 """
-
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    search = sys.argv[4]
-
-    # Connect to database
-    db = MySQLdb.connect(host='localhost',
-            user=username,
-            passwd=password,
-            db=database,
-            port=3306)
-
-    # Create cursor and execute parameterized query
+def connectToDB():
+    # Connecting to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (search,))
+    # Getting the state
+    statement = "SELECT * FROM states WHERE name = %s"
+    # Using parametrized query to avoid sql injection attacks
+    cur.execute(statement, [sys.argv[4]])
+    rows = cur.fetchall()
+    for item in rows:
+        print(item)
 
-    # Fetch and print results
-    for row in cur.fetchall():
-        print(row)
 
-    # Close cursor and database
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    connectToDB()
