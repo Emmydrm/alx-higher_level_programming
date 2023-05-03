f0af125f5b8b1b42a79b5fdf4892a0e5d3b257c8
Emmydrm <emmypee222@gmail.com>
2023-05-03 05:02:57 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a custom header variable
-curl -s -H "X-School-User-Id: 98" "$1"
+# Send a GET request to the provided URL with a header variable X-School-User-Id set to 98 using
+curl -sH "X-School-User-Id: 98" "$1"
