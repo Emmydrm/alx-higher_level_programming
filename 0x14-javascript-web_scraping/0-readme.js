2ab42388c5fdffc41164a95ed5111fd6c3296609
Emmydrm <emmypee222@gmail.com>
2023-06-07 14:27:08 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-
+// prints the content of a file
 const fs = require('fs');
 
-const filePath = process.argv[2];
+const filepath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
+fs.readFile(filepath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
     return;
   }
-
   console.log(data);
 });
