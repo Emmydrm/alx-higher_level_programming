d988a30580f79cba8b70686c8ef280c4675c9cdf
Emmydrm <emmypee222@gmail.com>
2023-06-07 14:39:47 +0000

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
-// writes a string to a file
-const fs = require('fs');
-const filepath = process.argv[2];
-const string = process.argv[3];
 
-fs.writeFile(filepath, string, (error) => {
-  if (error) {
-    console.error(error);
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+
+  console.log(`${filePath} has been successfully written.`);
 });
